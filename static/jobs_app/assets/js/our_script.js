0f920708da0954290bc106420df261be15b31d69
Kalu Kalu <1718221904@qq.com>
2017-06-09 00:16:05 +0800

    added post form and jsscript

---
+++ static/jobs_app/assets/js/our_script.js
@@ -0,0 +1,5 @@
+function showPostForm() {
+	var form = $('#post_form');
+	form.css('height', '180px');
+	form.css('opacity', '1');
+}
--- views.py
+++ views.py
@@ -4,4 +4,12 @@ from .models import JobPosting
 
 def home(request):
     job_posts = JobPosting.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        jp_obj = JobPosting.objects.create(
+            title=title,
+            description=description
+        )
+        jp_obj.save()
     return render(request, 'jobs_app/index.html', {'job_posts': job_posts})
